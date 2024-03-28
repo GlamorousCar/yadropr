6318dcf7ed3020df08f8dca159476d1273fb0f48
GlamorousCar <I89267857387@yandex.ru>
2024-03-28 14:35:53 +0300

    Выполнение задания

---
+++ Makefile
@@ -0,0 +1,8 @@
+
+build:
+	go build
+
+run:
+	$ go run . --s="follower brings bunch of questions"
+	$ go run . --s="i'll follow you as long as you are following me"
+
--- main.go
+++ main.go
@@ -1,5 +1,19 @@
 package main
 
+import (
+	"flag"
+	"fmt"
+	"strings"
+)
+
 func main() {
+	inputString := flag.String("s", "", "Предложение для стемминга")
+
+	flag.Parse()
+
+	stemmedWords := stemSentence(*inputString)
+	clearedSentence := clearSentence(stemmedWords)
+
+	fmt.Println(strings.Join(clearedSentence, " "))
 
 }
