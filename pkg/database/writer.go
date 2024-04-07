a0fd51c8cfd73ac2ff1305f2eeb07c80bdd63c7b
GlamorousCar <I89267857387@yandex.ru>
2024-04-08 02:42:05 +0300

    Удаление ненужного

--- pkg/database/writer.go
+++ pkg/database/writer.go
@@ -5,28 +5,14 @@ import (
 
 	"github.com/GlamorousCar/yadropr/pkg/words"
 	"github.com/GlamorousCar/yadropr/pkg/xkcd"
-	"io"
 	"io/ioutil"
 	"log"
 	"strconv"
-
-	"sync"
 )
-
-type SyncWriter struct {
-	m      sync.Mutex
-	Writer io.Writer
-}
 
 type ComicsContent struct {
 	URL      string   `json:"url"`
 	Keywords []string `json:"keywords"`
-}
-
-func (w *SyncWriter) Write(b []byte) (n int, err error) {
-	w.m.Lock()
-	defer w.m.Unlock()
-	return w.Writer.Write(b)
 }
 
 func WriteComicsToJson(filename string, comics []xkcd.Comics) {
